17c16497151d0921b1433fd4743332bec590b809
caique <caiquealves224@gmail.com>
2025-09-07 18:03:12 -0300

    feat: first implemtation of health and redirect

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,22 @@
-import { env } from "./utils/env.ts";
+import express, { type Request, type Response } from 'express';
+import { env } from './utils/env.ts';
 
-const hello: string = "Hello, World!!!";
+const app = express();
 
-console.log(hello, {  appname: env.APP_NAME });
+app.get('/', (req: Request, res: Response) => {
+  res.redirect('/health');
+});
+
+app.get('/health', (req: Request, res: Response) => {
+  try {
+    res.status(200).json({ success: true, message: 'Service is healthy' });
+  } catch (error) {
+    console.error('Health check failed:', error);
+    res.status(500).send('Server is not running');
+  }
+});
+
+app.listen(Number(env.PORT), () => {
+  console.log(`${env.APP_NAME} is running on ${env.NODE_ENV === 'development' ?
+    'http://localhost' : '0.0.0.0'}:${Number(env.PORT)} in ${env.NODE_ENV} mode.`);
+});
